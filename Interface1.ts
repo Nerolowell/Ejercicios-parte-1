7b88c746b806d488c2eabe67b6b36dbacab368a7
Nero Lowell <cuentaalexkiller@hotmail.com>
2019-08-26 00:55:25 -0600

    MI PRIMERA INTERFAZ

---
+++ Interface1.ts
@@ -0,0 +1,24 @@
+interface Lakes {    
+    nombre: string,    
+    area: number,    
+    largo: number,    
+    profundidad: number,    
+    esAguaDulce: boolean,    
+    paises: string[] 
+}
+let firstLake:Lakes={
+    nombre:'Oceano Caspio',
+    largo: 1199,
+    profundidad: 1025,
+    area: 371000,
+    esAguaDulce: false,
+    paises:['1','2','3','4','5']
+}
+let secondLake:Lakes={
+    nombre:'Superior',
+    largo: 1199,
+    profundidad: 1025,
+    area: 371000,
+    esAguaDulce: false,
+    paises: ['Canada','Estados Unidos']
+}
---
+++ interface1.js
@@ -0,0 +1,16 @@
+var firstLake = {
+    nombre: 'Oceano Caspio',
+    largo: 1199,
+    profundidad: 1025,
+    area: 371000,
+    esAguaDulce: false,
+    paises: ['1', '2', '3', '4', '5']
+};
+var secondLake = {
+    nombre: 'Superior',
+    largo: 1199,
+    profundidad: 1025,
+    area: 371000,
+    esAguaDulce: false,
+    paises: ['Canada', 'Estados Unidos']
+};
